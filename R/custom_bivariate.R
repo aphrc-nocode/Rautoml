8c93397d658addb15429ddb82ad22846935de179
John Lunalo <johnlunalo95@gmail.com>
2025-07-08 17:56:01 +0300

    bivariate plot update

--- R/custom_bivariate.R
+++ R/custom_bivariate.R
@@ -17,9 +17,13 @@
 bivariate_plot <- function(df, outcome = NULL, features = NULL,
                            title = "", colorbrewer = "Dark2", custom_theme = theme_minimal()){
   if (!is.null(outcome) && !is.null(features)) {
+    n= 100
+    max_colors <- RColorBrewer::brewer.pal.info[colorbrewer, "maxcolors"]
+      col_bre <- colorRampPalette(RColorBrewer::brewer.pal(max_colors, colorbrewer))(n)
+      
     p <- ggbivariate(df, outcome = outcome, explanatory = features, title = title) +
       custom_theme +
-      ggplot2::scale_fill_brewer(palette = colorbrewer) +
+      ggplot2::scale_fill_manual(values = col_bre) +
       ggplot2::theme(plot.title = ggplot2::element_text(size = 18, hjust = 0.5))
   } else {
     p <- ggplot2::ggplot() + custom_theme
@@ -31,5 +35,7 @@ bivariate_plot <- function(df, outcome = NULL, features = NULL,
 #mtcars1 <- mtcars
 #mtcars1$gear <- as.factor(mtcars1$gear)
 #mtcars1$am <- as.factor(mtcars1$am)
-#bivariate_plot(df = mtcars1, outcome = "am", features = names(mtcars1)[names(mtcars1)!="am"])
+#test <- bivariate_plot(df = mtcars1, outcome = "am", features = names(mtcars1)[names(mtcars1)!="am"])
+#ggbivariate(mtcars1, outcome = "am", explanatory = names(mtcars1)[names(mtcars1)!="am"], title = "")
+
 #bivariate_plot(df = mtcars1, outcome = "am", features = names(mtcars1)[names(mtcars1)!="am"], colorbrewer = "Accent")
